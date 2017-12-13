16786f3fe2513a48638a14964ad414b794fd70ab
Srawan Shrestha <shrestha.srawank@gmail.com>
2017-12-13 17:08:22 +0545

    added translations for messages

---
+++ src/lang/en/messages.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'create_success' => ':entity created successfully',
+    'update_success' => ':entity updated successfully',
+    'delete_success' => ':entity deleted successfully'
+];
