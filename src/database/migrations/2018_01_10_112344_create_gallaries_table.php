347fcf1cdbb016f106651600cda1872af67c4115
Saugat <darwin.saugat@gmail.com>
2018-01-17 17:23:50 +0545

    Change Gallery Table Engine to Innodb

--- src/database/migrations/2018_01_10_112344_create_gallaries_table.php
+++ src/database/migrations/2018_01_10_112344_create_gallaries_table.php
@@ -14,6 +14,7 @@ class CreateGallariesTable extends Migration
     public function up()
     {
         Schema::create('galleries', function (Blueprint $table) {
+            $table->engine = 'InnoDB';
             $table->increments('id');
             $table->integer('user_id');
             $table->string('title');
