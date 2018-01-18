697df68eccfa6ba1ab70dad6a02d4b61cde14cb3
Suraj Yogi <surajyogi910@gmail.com>
2018-01-18 13:40:49 +0545

    Return back with old data when validation error occured

--- src/views/admin/post/form.blade.php
+++ src/views/admin/post/form.blade.php
@@ -13,33 +13,33 @@
 
     <!-- Tab panes -->
     <div class="tab-content">
-        <input type="hidden" :value="post_type.id" name="post_type_id">
+        <input type="hidden" :value="post_type.id" name="post[post_type_id]">
         <div role="tabpanel" class="tab-pane active" id="main">
             <div class="form-group">
                 <label for="title" class="control-label col-sm-2">{{ fw_post_alias($postType, 'title', 'Post Title')  }}</label>
                 <div class="col-sm-10">
-                    <input class="form-control" name="title" type="text" v-model="post.title" id="title">
+                    <input class="form-control" name="post[title]" type="text" v-model="post.title" id="title">
                     <span class="help-block"></span>
                 </div>
             </div>
             <div class="form-group{{ fw_post_alias_visible($postType, 'sub_title') ? '': ' hidden' }}">
                 <label for="sub_title" class="control-label col-sm-2">{{ fw_post_alias($postType, 'sub_title', 'Post Sub Title')  }}</label>
                 <div class="col-sm-10">
-                    <input class="form-control" name="sub_title" type="text" v-model="post.sub_title" id="sub_title">
+                    <input class="form-control" name="post[sub_title]" type="text" v-model="post.sub_title" id="sub_title">
                     <span class="help-block"></span>
                 </div>
             </div>
             <div class="form-group{{ fw_post_alias_visible($postType, 'slug') ? '': ' hidden' }}">
                 <label for="slug" class="control-label col-sm-2">Slug</label>
                 <div class="col-sm-10">
-                    <input class="form-control" name="slug" type="text" v-model="post.slug" id="slug" debounce="500">
+                    <input class="form-control" name="post[slug]" type="text" v-model="post.slug" id="slug" debounce="500">
                     <span class="help-block">Appears on url</span>
                 </div>
             </div>
             <div class="form-group{{ fw_post_alias_visible($postType, 'view') ? '': ' hidden' }}">
                 <label for="view" class="control-label col-sm-2">{{ fw_post_alias($postType, 'view', 'Template')  }}</label>
                 <div class="col-sm-10">
-                    <select class="form-control" name="view" v-model="post.view">
+                    <select class="form-control" name="post[view]" v-model="post.view">
                         <option v-for="t in templates" :value="t">@{{ t }}</option>
                     </select>
                     <span class="help-block">Filename of Blade Template File</span>
@@ -48,31 +48,31 @@
             <div class="form-group{{ fw_post_alias_visible($postType, 'content') ? '': ' hidden' }}">
                 <label for="content" class="control-label col-sm-2">{{ fw_post_alias($postType, 'content', 'Post Content')  }}</label>
                 <div class="col-sm-10">
-                    <ckeditor name="content" id="content" v-model="post.content" class="editor" :config="editor.full"></ckeditor>
+                    <ckeditor name="post[content]" id="content" v-model="post.content" class="editor" :config="editor.full"></ckeditor>
                     <span class="help-block">Main Content of the Post</span>
                 </div>
             </div>
             <div class="form-group" v-for="(field,i) in post.custom">
                 <label :for="'custom-'+i+'-value'" class="control-label col-sm-2">@{{ field.title }}</label>
                 <div class="col-sm-10">
-                    <input :name="'custom['+i+'][slug]'" type="hidden" v-model="field.slug">
-                    <input :name="'custom['+i+'][type]'" type="hidden" v-model="field.type">
-                    <input :name="'custom['+i+'][title]'" type="hidden" v-model="field.title">
-                    <input :name="'custom['+i+'][options]'" type="hidden" v-model="field.options">
-                    <input class="form-control" :name="'custom['+i+'][value]'" type="text" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='raw-text'">
-                    <input class="form-control" :name="'custom['+i+'][value]'" type="number" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='number'">
-                    <input class="form-control" :name="'custom['+i+'][value]'" type="date" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='date'">
-                    <select class="form-control" :name="'custom['+i+'][value]'" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='select'">
+                    <input :name="'post[custom]['+i+'][slug]'" type="hidden" v-model="field.slug">
+                    <input :name="'post[custom]['+i+'][type]'" type="hidden" v-model="field.type">
+                    <input :name="'post[custom]['+i+'][title]'" type="hidden" v-model="field.title">
+                    <input :name="'post[custom]['+i+'][options]'" type="hidden" v-model="field.options">
+                    <input class="form-control" :name="'post[custom]['+i+'][value]'" type="text" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='raw-text'">
+                    <input class="form-control" :name="'post[custom]['+i+'][value]'" type="number" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='number'">
+                    <input class="form-control" :name="'post[custom]['+i+'][value]'" type="date" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='date'">
+                    <select class="form-control" :name="'post[custom]['+i+'][value]'" v-model="field.value" :id="'custom-'+i+'-value'" v-if="field.type=='select'">
                         <option v-for="option in field.options.split(/\r?\n/)" :value="option" v-html="option"></option>
                     </select>
-                    <ckeditor :name="'custom['+i+'][value]'" :id="'custom-'+i+'-value'" v-model="field.value" class="editor mini" v-if="field.type=='formatted-text'" :config="editor.mini"></ckeditor>
+                    <ckeditor :name="'post[custom]['+i+'][value]'" :id="'custom-'+i+'-value'" v-model="field.value" class="editor mini" v-if="field.type=='formatted-text'" :config="editor.mini"></ckeditor>
                     <span class="help-block"></span>
                 </div>
             </div>
             <div class="form-group{{ fw_post_alias_visible($postType, 'tags') ? '': ' hidden' }}">
                 <label for="post_type_id" class="control-label col-sm-2">Tags</label>
                 <div class="col-sm-10">
-                    <select class="form-control select2" name="tags[]" multiple>
+                    <select class="form-control select2" name="post[tags][]" multiple>
                         <option v-for="tag in tags" :value="tag" :selected="post.tags.filter(t => t.title == tag).length">@{{ tag }}</option>
                     </select>
                     <span class="help-block"></span>
@@ -82,11 +82,11 @@
                 <label for="is_published" class="control-label col-sm-2">{{ fw_post_alias($postType, 'is_published', 'Published')  }}</label>
                 <div class="col-sm-10">
                     <label>
-                        <input type="radio" name="is_published" id="is_published" :value="true" v-model="post.is_published">
+                        <input type="radio" name="post[is_published]" id="is_published" :value="true" v-model="post.is_published">
                         Yes
                     </label>
                     <label>
-                        <input type="radio" name="is_published" id="is_published" :value="false" v-model="post.is_published">
+                        <input type="radio" name="post[is_published]" id="is_published" :value="false" v-model="post.is_published">
                         No
                     </label>
                 </div>
@@ -95,18 +95,18 @@
                 <label for="is_featured" class="control-label col-sm-2">{{ fw_post_alias($postType, 'is_featured', 'Featured')  }}</label>
                 <div class="col-sm-10">
                     <label>
-                        <input type="radio" name="is_featured" id="is_featured" :value="true" v-model="post.is_featured">
+                        <input type="radio" name="post[is_featured]" id="is_featured" :value="true" v-model="post.is_featured">
                         Yes
                     </label>
                     <label>
-                        <input type="radio" name="is_featured" id="is_featured" :value="false" v-model="post.is_featured">
+                        <input type="radio" name="post[is_featured]" id="is_featured" :value="false" v-model="post.is_featured">
                         No
                     </label>
                 </div>
             </div>
         </div>
         <div role="tabpanel" class="tab-pane" id="image" v-if="post.images.length">
-            <input v-for="img in deleted_image_ids" name="deleted_image_ids[]" type="hidden" :value="img">
+            <input v-for="img in deleted_image_ids" name="post[deleted_image_ids][]" type="hidden" :value="img">
             <div v-for="(field,i) in post.images" class="panel">
                 <div class="panel-body">
                     <div class="row">
@@ -124,13 +124,13 @@
                                     <div class="form-group">
                                         <label :for="'images['+i+'][file]'">Image Source:</label>
                                         <div class="input-group">
-                                            <input v-if="field.id" :name="'images['+i+'][image_id]'" type="hidden" :value="field.id">
-                                            <image-selector :name="'images['+i+'][id]'" :file-name="'images['+i+'][file]'" @change="value => field.thumbnail = value" :id="'images['+i+'][file]'"/>
+                                            <input v-if="field.id" :name="'post[images]['+i+'][image_id]'" type="hidden" :value="field.id">
+                                            <image-selector :name="'post[images]['+i+'][id]'" :file-name="'images['+i+'][file]'" @change="value => field.thumbnail = value" :id="'images['+i+'][file]'"/>
                                         </div>
                                     </div>
                                     <div class="form-group hidden">
                                         <label :for="'images['+i+'][slug]'">Image Slug: </label>
-                                        <input class="form-control" :name="'images['+i+'][pivot][slug]'" type="text" v-model="field.pivot.slug">
+                                        <input class="form-control" :name="'post[images]['+i+'][pivot][slug]'" type="text" v-model="field.pivot.slug">
                                     </div>
                                 </div>
                             </div>
@@ -143,21 +143,21 @@
             <div class="form-group">
                 <label for="meta_title" class="control-label col-sm-2">Meta Title</label>
                 <div class="col-sm-10">
-                    <input class="form-control" name="meta_title" type="text" v-model="post.meta_title" id="meta_title">
+                    <input class="form-control" name="post[meta_title]" type="text" v-model="post.meta_title" id="meta_title">
                     <span class="help-block"></span>
                 </div>
             </div>
             <div class="form-group">
                 <label for="meta_description" class="control-label col-sm-2">Meta Description</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" name="meta_description" id="meta_description" v-model="post.meta_description"></textarea>
+                    <textarea class="form-control" name="post[meta_description]" id="meta_description" v-model="post.meta_description"></textarea>
                     <span class="help-block"></span>
                 </div>
             </div>
             <div class="form-group">
                 <label for="meta_keywords" class="control-label col-sm-2">Meta Keywords</label>
                 <div class="col-sm-10">
-                    <textarea class="form-control" name="meta_keywords" id="meta_keywords" v-model="post.meta_keywords"></textarea>
+                    <textarea class="form-control" name="post[meta_keywords]" id="meta_keywords" v-model="post.meta_keywords"></textarea>
                     <span class="help-block">Separate keywords with a comma(,)</span>
                 </div>
             </div>
@@ -173,18 +173,15 @@
     <script src="https://cdn.ckeditor.com/4.7.3/standard/ckeditor.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.5/js/select2.full.min.js"></script>
     <script>
-                @if(isset($post))
-        let post = JSON.parse('{!! addslashes(json_encode($post)) !!}');
-                @endif
-                @if(!empty($postType))
-        let post_type = JSON.parse('{!! addslashes(json_encode($postType)) !!}');
-                @endif
-                {{--@if($p = old('post', isset($post)?$post:null))--}}
-                {{--let post = JSON.parse('{!! addslashes(json_encode($p)) !!}');--}}
-                {{--@endif--}}
-                @if(isset($tags))
-        let tags = JSON.parse('{!! addslashes(json_encode($tags)) !!}');
-                @endif
+        @if($p = old('post', isset($post) ? $post : null))
+            let post = JSON.parse('{!! addslashes(json_encode($p)) !!}');
+        @endif
+        @if(!empty($postType))
+            let post_type = JSON.parse('{!! addslashes(json_encode($postType)) !!}');
+        @endif
+        @if(isset($tags))
+            let tags = JSON.parse('{!! addslashes(json_encode($tags)) !!}');
+        @endif
         let post_types = JSON.parse('{!! addslashes(json_encode($postTypes)) !!}');
         let templates = JSON.parse('{!! addslashes(json_encode($templates)) !!}');
         $(document).ready(function () {
